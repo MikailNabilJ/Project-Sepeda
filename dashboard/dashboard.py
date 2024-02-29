66aed30f5f90c3eeea2899db165f274528e0ab4d
MIKAIL NABIL JORDAN <138708367+MikailNabilJ@users.noreply.github.com>
2024-03-01 04:00:18 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+from sklearn.preprocessing import StandardScaler
 
 # Set tema seaborn
 sns.set_theme(style='dark')
